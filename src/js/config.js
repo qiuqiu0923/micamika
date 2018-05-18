6fd21393fcaab06a5a6f55b2cdd4ee9f542abb84
likun <285781031@qq.com>
2018-05-18 20:58:35 +0800

    jinwan

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,23 @@
 require.config({
-    baseUrl: "/",    
-    paths:{
-        jquery:"lib/jquery/"
+    baseUrl : "/",
+    paths : {
+        jquery : "lib/jquery/jquery-1.12.4.min",
+        artTemplate : "lib/artTemplate/template-web",
+        cookie : "lib/jquery-plugins/jquery.cookie",
+        fly : "lib/jquery-plugins/jquery.fly.min",
+        zoom : "lib/jquery-plugins/jquery.elevateZoom-3.0.8.min",
+        load : "js/loadHeadAndFooter",
+        bootstrap : "lib/bootstrap/js/.min.js"
+    },
+    shim : {
+        fly : {
+            deps : ["jquery"]
+        },
+        zoom : {
+            deps : ["jquery"]
+        },
+        bootstrap: {
+            deps : ["jquery"]
+        }
     }
-})
+});
